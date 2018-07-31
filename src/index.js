8a1b36c5740adb3544f31221fc59e3164868fed1
myProject <15292252509@163.com>
2018-07-31 17:10:42 +0800

    v0.0.1 初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("git project!");
+class FastCache {
+  constructor() {
+    this.list = {}
+  }
+  set(key,value) {
+    this.list[key] = value
+  }
+  get(key) {
+    return this.list[key]
+  }
+}
+
+window.FastCache = FastCache
